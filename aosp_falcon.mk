d384151885f3fb80fe186e5f8ceaba854101e632
ricksanchez17 <34612791+ricksanchez777@users.noreply.github.com>
2018-02-06 18:28:05 -0200

    AOSP bringup

--- aosp_falcon.mk
+++ aosp_falcon.mk
@@ -1,9 +1,41 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Inherit from those products. Most specific first.
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+TARGET_ARCH := arm
+TARGET_DENSITY := xhdpi
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_MINIMAL_APPS := true
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := lineage_falcon
+# Boot animation
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
 
-PRODUCT_GMS_CLIENTID_BASE := android-motorola
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := falcon
+PRODUCT_NAME := aosp_falcon
+PRODUCT_BRAND := motorola
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="Moto G"
