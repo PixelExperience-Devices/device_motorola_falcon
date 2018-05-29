8ecdad0f255927668ec1c2abee12f40c2aea85d8
ricksanchez17 <34612791+ricksanchez777@users.noreply.github.com>
2018-05-29 20:41:19 -0300

    Update aosp_falcon.mk

--- aosp_falcon.mk
+++ aosp_falcon.mk
@@ -1,44 +1,12 @@
-#
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
-# Inherit from those products. Most specific first.
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
-# Inherit some common AOSP stuff.
-TARGET_ARCH := arm
-TARGET_DENSITY := xhdpi
+# Inherit some common PixelExperience stuff.
+TARGET_GAPPS_ARCH := arm
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_MINIMAL_APPS := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# Boot animation
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1280
-
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := falcon
+PRODUCT_RELEASE_NAME := falcon
 PRODUCT_NAME := aosp_falcon
-PRODUCT_BRAND := motorola
-PRODUCT_MANUFACTURER := motorola
 
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="Moto G"
-
-# Inherit common Android Go configurations
-#$(call inherit-product, build/target/product/go_defaults.mk)
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
