b9022b2800eb88fe7defbfc67270dbada9a071c8
Danny Baumann <dannybaumann@web.de>
2018-08-31 13:34:18 +0200

    titan: Load dummy initial BT MAC address.
    The real one will be loaded by hci_qcomm_init, but the BT HAL wants an
    address to be set before that point.
    
    Change-Id: Ia8ff499ccaa862ad697dcfecb969b29f29ee89f4

--- init/init_falcon.cpp
+++ init/init_falcon.cpp
@@ -168,4 +168,7 @@ void vendor_load_properties()
 
     std::string device = GetProperty("ro.product.device", "");
     LOG(INFO) << "Found radio id: '" << radio.c_str() << "', setting build properties for '" << device.c_str() << "' device\n";
+	
+	// Init a dummy BT MAC address, will be overwritten later
+    property_set("ro.boot.btmacaddr", "00:00:00:00:00:00");
 }
