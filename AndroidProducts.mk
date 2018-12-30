e596d023b4dc3d64d4eef30ce4300427c2a50f77
Rohan Hasabe <43720061+Hasaber8@users.noreply.github.com>
2018-12-30 15:06:01 +0530

    Update dependencies for Pie

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_falcon.mk \
-    $(LOCAL_DIR)/lineage_falcon.mk
+    $(LOCAL_DIR)/rr_falcon.mk
--- rr_falcon.mk
+++ rr_falcon.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common RRp stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := lineage_falcon
+PRODUCT_NAME := rr_falcon
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
